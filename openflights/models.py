361449e7ad477055142ff99105731e9570226592
David Gouldin <david@gould.in>
2013-12-04 20:35:30 -0800

    Adding app directory and stub models.

---
+++ openflights/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Airport(models.Model):
+    pass
+
+class Airline(models.Model):
+    pass
+
+class Route(models.Model):
+    pass
+
+class Schedule(models.Model):
+    pass
